bf5772583885a782987ae6239b0aa3b37296ee64
Clamel05 <caml@bell.net>
2024-09-17 14:11:32 -0400

    Added movement to the player

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,27 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+
     void Update()
     {
+        Vector3 offset = Vector3.zero;
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+            offset += Vector3.left * 0.01f;
+        if (Input.GetKey(KeyCode.RightArrow))
+            offset += Vector3.right * 0.01f;
+        if (Input.GetKey(KeyCode.UpArrow))
+            offset += Vector3.up * 0.01f;
+        if (Input.GetKey(KeyCode.DownArrow))
+            offset += Vector3.down * 0.01f;
+
+        PlayerMovevement(offset);
+
+    }
+
+    private void PlayerMovevement(Vector3 offset)
+    {
+        transform.position += offset;
     }
 
 }
