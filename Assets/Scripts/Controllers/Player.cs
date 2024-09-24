59ecc2f1e2733d0744b33ad62f49ed47c5cfc1d0
Clamel05 <caml@bell.net>
2024-09-23 23:10:14 -0400

    Task 1c complete
    Created a deceleration if statement so when there is no input the speed decreases to 0.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,28 +9,69 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
-    public float Speed = 5.0f;
-    float accelrationTime = 5f;
-    float time = 0f;
+    public float Speed = 0.0f;
+    //float accelrationTime = 5f;
+    //float time = 0f;
+    public float MaxSpeed = 20.0f;
 
     void Update()
     {
         Vector3 offset = Vector3.zero;
 
-        
+        /*for(time = 0f; time < accelrationTime * Time.deltaTime; time += Speed)
+        {
+            for (MaxSpeed = 20.0f; Speed < MaxSpeed; Speed++)
+            {
 
-        
-         
+            }
+        }*/
+
+
 
         if (Input.GetKey(KeyCode.LeftArrow))
+        {
             offset += Vector3.left * Speed;
-        if (Input.GetKey(KeyCode.RightArrow))
+            for (MaxSpeed = 20.0f; Speed < MaxSpeed; Speed++) ;
+        }
+        else
+            if (Speed > 0)
+        {
+            Speed--;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
             offset += Vector3.right * Speed;
+            for (MaxSpeed = 20.0f; Speed < MaxSpeed; Speed++) ;
+        }
+        else
+            if (Speed > 0)
+        {
+            Speed--;
+        }
+
         if (Input.GetKey(KeyCode.UpArrow))
+        {
             offset += Vector3.up * Speed;
+            for (MaxSpeed = 20.0f; Speed < MaxSpeed; Speed++) ;
+        }
+        else
+            if(Speed > 0)
+        {
+            Speed--;
+        }
+            
+
         if (Input.GetKey(KeyCode.DownArrow))
+        {
             offset += Vector3.down * Speed;
-        
+            for (MaxSpeed = 20.0f; Speed < MaxSpeed; Speed++) ;
+        }
+        else
+            if (Speed > 0)
+        {
+            Speed--;
+        }
+
 
         PlayerMovevement(offset);
 
@@ -38,14 +79,7 @@ public class Player : MonoBehaviour
 
     private void PlayerMovevement(Vector3 offset)
     {
-        for (time = 0f; time < accelrationTime * Time.deltaTime; time += Speed)
-        {
-            for (float MaxSpeed = 20.0f; Speed < MaxSpeed; Speed++)
-            {
-                transform.position += offset * Time.deltaTime;
-            }
-        }
-        
+        transform.position += offset * Time.deltaTime;
     }
 
 }
